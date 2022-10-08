12c419883d2f22ff61dc1abd9df6b130f7a8350d
lingmu415 <73768882+lingmu415@users.noreply.github.com>
2022-10-09 01:34:58 +0900

    Update task1.sol

--- task1.sol
+++ task1.sol
@@ -9,8 +9,9 @@ contract task1 {
         uint256 percentage;
         uint256 totalMark;
     }
-    Student[] public students;
+
     address payable owner;
+    
     mapping(address => Student) addressToStundent;
 
     modifier onlyOwner(){
@@ -18,19 +19,22 @@ contract task1 {
         _;
     }
 
+
     constructor() {
         owner = payable(msg.sender);
     }
 
+
    function getStudentDetails(address studentID) public view returns(Student memory){
-    return addressToStundent[studentID];
+       return addressToStundent[studentID];
    }
 
+
    function register(address _studentID, uint256 _percentage, uint256 _totalMark) public onlyOwner {
-    //cannot create with a same address
-    require(!addressToStundent[_studentID].IsRegistered,"already registered");
-    //Create a new Student
-    Student memory _student = Student(true, _studentID, _percentage, _totalMark);
-    addressToStundent[_studentID] = _student;
+       //cannot create with a same address
+       require(!addressToStundent[_studentID].IsRegistered,"already registered");
+       //Create a new Student
+       Student memory _student = Student(true, _studentID, _percentage, _totalMark);
+       addressToStundent[_studentID] = _student;
    }
 }
